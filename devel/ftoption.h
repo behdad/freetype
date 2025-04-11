ee7d7863027d5de5bdd452e6a5fc41b0042304a3
Behdad Esfahbod <behdad@behdad.org>
2025-04-11 16:49:00 -0600

    [harfbuzz] Towards actually working with dlopen()

--- devel/ftoption.h
+++ devel/ftoption.h
@@ -292,6 +292,8 @@ FT_BEGIN_HEADER
    */
 #define FT_CONFIG_OPTION_USE_HARFBUZZ
 
+  /* XXX(harfbuzz) */
+#define FT_CONFIG_OPTION_USE_HARFBUZZ_DYNAMIC
 
   /**************************************************************************
    *
--- src/autofit/ft-hb.c
+++ src/autofit/ft-hb.c
@@ -27,36 +27,39 @@
 #include <freetype/freetype.h>
 #include <freetype/internal/ftmemory.h>
 
-#ifdef FT_CONFIG_OPTION_USE_HARFBUZZ
+#if defined(FT_CONFIG_OPTION_USE_HARFBUZZ) && \
+    defined(FT_CONFIG_OPTION_USE_HARFBUZZ_DYNAMIC)
 
 #include "ft-hb.h"
 
+#include "afglobal.h"
+
 #include <dlfcn.h>
 
-ft_hb_funcs_t * ft_hb_funcs (void)
+ft_hb_funcs_t * ft_hb_funcs_new ( struct  AF_FaceGlobalsRec_ *af_globals )
 {
-  static ft_hb_funcs_t *funcs = NULL;
-  if (funcs) return funcs;
+  FT_Memory memory = af_globals->face->memory;
+  FT_Error error;
+  ft_hb_funcs_t *funcs = NULL;
 
-#if 0
   if ( FT_NEW ( funcs ) )
     return NULL;
-#endif
+  FT_ZERO( funcs );
 
-  funcs->lib = NULL;
-  ft_hb_version_at_least_func_t version_at_least = dlsym( RTLD_DEFAULT, "hb_version_at_least" );
-  if ( !version_at_least ) {
+  ft_hb_version_atleast_func_t version_atleast = dlsym( RTLD_DEFAULT, "hb_version_atleast" );
+  if ( !version_atleast )
+  {
     /* Load the HarfBuzz library */
-    funcs->lib = dlopen( "libharfbuzz.so", RTLD_GLOBAL );
+    funcs->lib = dlopen( "libharfbuzz.so.0", RTLD_LAZY | RTLD_GLOBAL );
     if ( !funcs->lib )
       goto fail;
-    version_at_least = dlsym( funcs->lib, "hb_version_at_least" );
-    if ( !version_at_least )
+    version_atleast = dlsym( funcs->lib, "hb_version_atleast" );
+    if ( !version_atleast )
       goto fail;
   }
 
   /* Keep version in sync with meson.build and configure.raw */
-  if ( !version_at_least ( 2, 0, 0 ) )
+  if ( !version_atleast ( 2, 0, 0 ) )
     goto fail;
 
   /* Load all symbols we use. */
@@ -72,21 +75,37 @@ ft_hb_funcs_t * ft_hb_funcs (void)
   return funcs;
 
 fail:
-  if (funcs->lib)
-    dlclose (funcs->lib);
-#if 0
-  if (funcs)
-    FT_FREE (funcs);
-#endif
-  funcs = NULL;
+  ft_hb_funcs_free ( funcs, af_globals );
   return NULL;
 }
 
-#else /* !FT_CONFIG_OPTION_USE_HARFBUZZ */
+void ft_hb_funcs_free ( ft_hb_funcs_t* funcs, struct AF_FaceGlobalsRec_ *af_globals)
+{
+  FT_Memory memory = af_globals->face->memory;
+
+  if ( !funcs )
+    return;
+
+#if 0
+  /* Never close the library, since we opened it with RTLD_GLOBAL.
+   * This is important for the case where we are using HarfBuzz
+   * as a shared library, and we want to use the symbols from
+   * the library in other shared libraries or clients. HarfBuzz
+   * holds onto global variables, and closing the library
+   * will cause them to be invalidated.
+   */
+  if ( funcs->lib )
+    dlclose ( funcs->lib );
+#endif
+
+  FT_FREE ( funcs );
+}
+
+#else /* !FT_CONFIG_OPTION_USE_HARFBUZZ* */
 
 /* ANSI C doesn't like empty source files */
 typedef int  ft_hb_dummy_;
 
-#endif /* !FT_CONFIG_OPTION_USE_HARFBUZZ */
+#endif /* !FT_CONFIG_OPTION_USE_HARFBUZZ* */
 
 /* END */
