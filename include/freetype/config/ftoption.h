ec2abb4fa875149237958f1d0b655ca8da461d73
Behdad Esfahbod <behdad@behdad.org>
2025-04-12 13:24:57 -0600

    [harfbuzz] Fix build

--- include/freetype/config/ftoption.h
+++ include/freetype/config/ftoption.h
@@ -295,6 +295,23 @@ FT_BEGIN_HEADER
 
   /**************************************************************************
    *
+   * HarfBuzz dynamic support.
+   *
+   *   Define this macro if you want the HarfBuzz library to be loaded at
+   *   runtime instead of being linked to FreeType.
+   *
+   *   This option has no effect if `FT_CONFIG_OPTION_USE_HARFBUZZ` is not defined.
+   *
+   *   If you use a build system like cmake or the `configure` script,
+   *   options set by those programs have precedence, overwriting the value
+   *   here with the configured one.
+   */
+/* #define FT_CONFIG_OPTION_USE_HARFBUZZ_DYNAMIC */
+
+
+
+  /**************************************************************************
+   *
    * Brotli support.
    *
    *   FreeType uses the Brotli library to provide support for decompressing
